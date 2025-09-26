252cd54a3eb1ad6b68c0c95d6505592fff82b3af
ShivamCo <shivam.singh.coo@gmail.com>
2025-09-26 11:54:27 +0530

    added port

--- server/server.js
+++ server/server.js
@@ -16,12 +16,12 @@ import deleteDocumentsRoute from "./routes/deleteDocumentRoute.js"
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend URL
+  origin: "http://localhost:5173", 
   credentials: true,
 }));
 
