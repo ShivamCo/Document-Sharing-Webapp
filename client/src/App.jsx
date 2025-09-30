47ed835c7d6628ed5ee3b0c1d45ae4c26ed9886e
ShivamCo <shivam.singh.coo@gmail.com>
2025-09-30 17:45:01 +0530

    updated pin verification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,21 +10,7 @@ import axios from "axios";
 
 function App() {
 const API_URL = import.meta.env.VITE_API_URL;
-  const wakeServer = () =>{
 
-    try {
-
-      const res = axios.get(`${API_URL}/test-router`);
-
-
-    } catch (error){
-        console.log(error)
-    }
-
-
-  }
-
-  setTimeout(wakeServer(), 1000 * 60 * 9 )
   
   return (
     <BrowserRouter>
--- client/src/components/dashboard/QrCodeGenrator.jsx
+++ client/src/components/dashboard/QrCodeGenrator.jsx
@@ -3,11 +3,11 @@ import {QRCodeCanvas} from 'qrcode.react';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const QrCodeGenrator = ({adminId}) =>{
-    console.log(`/upload/${adminId}`)
+    
     return(
         <div>
     <QRCodeCanvas
-        value={`${API_URL}/upload/${adminId}`}
+        value={`https://print-doc-manager.netlify.app/upload/${adminId}`}
         size={256}
         bgColor="#ffffff"
         fgColor="#111827"
